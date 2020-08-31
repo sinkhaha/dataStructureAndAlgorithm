5fc5195be6eb5113adeebb27b7923202d4cbc913
sink <1468709106@qq.com>
2020-08-31 21:02:54 +0800

    3

--- dataStructure/search/二分查找变形三.js
+++ dataStructure/search/二分查找变形三.js
@@ -1,4 +1,8 @@
-// 变形三：查找第一个大于等于给定值的元素
+/**
+ * 变形三：查找第一个大于等于给定值的元素
+ * @param {*} a 
+ * @param {*} target 
+ */
 const binaryFindFistBig = (a, target) => {
     if (a.length === 0) {
         return -1;
@@ -8,15 +12,16 @@ const binaryFindFistBig = (a, target) => {
     let high = a.length - 1;
     while (low <= high) {
         const mid =  Math.floor(low+(high-low)/2);
-        if (target <= a[mid]) {
-            // mid此时是数组第一个元素 或者 前一个值小于要查找的值，那此时就是该值了
+        if (a[mid] >= target) {
+            // 如果mid此时是数组第一个元素 或者 前一个值小于要查找的值，那此时就是要查找的第一个大于target的值了
             if (mid === 0 || a[mid - 1] < target) {
                 return mid;
             } else {
+                // 在[low, mid-1]找
                 high = mid - 1;
             }
         } else {
-            // 如果a[mid]小于要查找的值 value，那要查找的值肯定在[mid+1, high]之间，所以更新 low=mid+1    
+            // 如果a[mid]小于要查找的值target, 那要查找的值肯定在[mid+1, high]之间，所以更新 low=mid+1    
             low = mid + 1;
         }
     }
