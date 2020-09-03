b52e1429c873a86d14cf571e1ed1f9babeafddb3
sink <1468709106@qq.com>
2020-09-03 21:56:47 +0800

    strStr

--- leetcode/双指针/strStr.js
+++ leetcode/双指针/strStr.js
@@ -1,11 +1,20 @@
 /**
+ * leetcode 28 easy
+ * 
+ * 实现 strStr() 函数:
+ * 给定一个 txt 字符串和一个 pat 字符串，在 txt 字符串中找出 pat 字符串出现的
+ * 第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ * 输入: txt = "hello", pat = "ll"
+ * 输出: 2
+ * 
  * 暴力解法：
  * 时间复杂度 O(MN)，空间复杂度O(1)
  * @param {string} txt
  * @param {string} pat
  * @return {number}
  */
-var strStr = function(txt, pat) {
+var strStr1 = function(txt, pat) {
     let N = txt.length;
     let M = pat.length;
 
@@ -26,4 +35,4 @@ var strStr = function(txt, pat) {
 
 const haystack = 'hello';
 const needle = 'll';
-console.log(strStr(haystack, needle));
+console.log(strStr1(haystack, needle)); // 2
