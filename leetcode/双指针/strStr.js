66742931bff875837288d7daad1baf0e9ac1b9d5
sink <1468709106@qq.com>
2020-09-03 22:51:47 +0800

    strStr

--- leetcode/双指针/strStr.js
+++ leetcode/双指针/strStr.js
@@ -20,6 +20,11 @@ var strStr1 = function(txt, pat) {
     let N = txt.length;
     let M = pat.length;
 
+    // 都为空，返回0
+    if (!N && !M || (N === M)) {
+        return 0;
+    }
+
     for (let i = 0; i < N - M; i++) {
         let j;
         for (j = 0; j < M; j++) {
@@ -37,11 +42,46 @@ var strStr1 = function(txt, pat) {
     return -1;
 };
 
-const txt = 'hello';
-const pat = 'll';
-console.log(strStr1(txt, pat)); // 2
+const txt1 = '';
+const pat1 = 'a';
+console.log(strStr1(txt1, pat1)); // -1
 
-// kmp
+const txt2 = 'hello';
+const pat2 = 'll';
+console.log(strStr1(txt2, pat2)); // 2
+
+const txt3 = '';
+const pat3 = '';
+console.log(strStr1(txt3, pat3)); // 0
+
+const txt4 = 'a';
+const pat4 = 'a';
+console.log(strStr1(txt4, pat4)); // 0
+
+/**
+ * 
+ * 时间复杂度：O((N - L)L)，其中 N 为 txt 字符串的长度，L 为 pat 字符串的长度。
+ * 内循环中比较字符串的复杂度为 L，总共需要比较 (N - L) 次
+ *
+ * 空间复杂度：O(1)O(1)
+ *
+ * @param {*} txt 
+ * @param {*} pat 
+ */
 var strStr2 = function(txt, pat) {
-    
-};
+    let L = pat.length;
+    let n = txt.length;
+
+    for (let j = 0; j < n - L + 1; ++j) {
+      const sub = txt.substring(j, j + L);
+      if (sub == pat) {
+        return j;
+      }
+    }
+    return -1;
+}
+
+console.log(strStr2(txt1, pat1)); // -1
+console.log(strStr2(txt2, pat2)); // 2
+console.log(strStr2(txt3, pat3)); // 0
+console.log(strStr2(txt4, pat4)); // 0
