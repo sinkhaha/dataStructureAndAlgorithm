5a112f699f2551d6b72e4f7d4029509e53b084c1
sink <1468709106@qq.com>
2020-09-04 22:53:29 +0800

    window

--- leetcode/滑动窗口/minWindow.js
+++ leetcode/滑动窗口/minWindow.js
@@ -1,13 +1,30 @@
 /**
+ * leetcode 76 最小覆盖子串
+ * 
+ * 时间复杂度O(N) N为source度长度
+ * 空间复杂度O(M) M为target度长度
+ * 
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
 var minWindow = function(source, target) {
+    if (source.length < target.length) {
+        return null;
+    }
+    if (source.includes(target)) {
+        return target;
+    }
+
     // key是要匹配的字符，value值是1，即出现的次数
     const needs = new Map();
     for (let t of target) {
-        needs.set(t, 1);
+        const v = needs.get(t);
+        if (v === undefined) {
+            needs.set(t, 1);
+        } else {
+            needs.set(t, v + 1);
+        }
     }
    
     console.log(needs);
@@ -28,7 +45,7 @@ var minWindow = function(source, target) {
     while (right < source.length) {
         let rightLetter = source[right];
                 
-        // 进行窗口的数据更新，当前字符是需要的则加入窗口
+        // 1.进行窗口的数据更新，当前字符是需要的则加入窗口
         if (needs.has(rightLetter)) {
             const count = window.get(rightLetter); // key不存在返回undefined
             if (count === undefined) {
@@ -37,7 +54,7 @@ var minWindow = function(source, target) {
                 window.set(rightLetter, count + 1);
             }
 
-            // 如果相等则有效计数+1，排除掉窗口中值大于1的，即有重复的字符
+            // 2.如果相等则有效计数+1，排除掉窗口中值大于1的，即有重复的字符
             if (needs.get(rightLetter) === window.get(rightLetter)) {
                 isValidCount++;
             }
@@ -45,7 +62,7 @@ var minWindow = function(source, target) {
 
         right++; // 右移动窗口
 
-        // 左移动窗口，window已经包含了needs所有的字符
+        // 3.左移动窗口，window已经包含了needs所有的字符
         while (isValidCount === needs.size) {
             // 更新最小覆盖字串
             const curLen = right - left;
@@ -56,11 +73,11 @@ var minWindow = function(source, target) {
 
             let l = source[left];
             if (needs.has(l)) {
-                // 即将窗口左移动后，窗口已经不包含需要的字符了，有效计数需要减一
+                // 4.即将窗口左移动后，窗口已经不包含需要的字符了，有效计数需要减一
                 if (window.get(l) === needs.get(l)) {
                     isValidCount--;
                 }
-                
+
                 const curWinValue = window.get(l);
                 if (curWinValue !== undefined) {
                     window.set(l, curWinValue - 1);
@@ -72,14 +89,28 @@ var minWindow = function(source, target) {
         }
     }
 
-    console.log(`start=${start} len=${len}`);
+    // console.log(`start=${start} len=${len}`);
 
     return len === Number.MAX_VALUE 
-        ? ''
-        :  source.substr(start, len);
+        ? null
+        : source.substr(start, len);
 };
 
 const source = 'ADOBECODEBANC';
 const target = 'ABC';
-
 console.log(minWindow(source, target));
+console.log('=============================');
+
+const source1 = 'a';
+const target1 = 'aa';
+console.log(minWindow(source1, target1));
+console.log('=============================');
+
+const source2 = 'aa';
+const target2 = 'aa';
+console.log(minWindow(source2, target2));
+console.log('=============================');
+
+const source3 = 'bbaa';
+const target3 = 'aba';
+console.log(minWindow(source3, target3));
