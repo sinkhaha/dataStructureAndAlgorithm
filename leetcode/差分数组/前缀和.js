518d9e6c9ef478b8a14a00fa08312d6e302ec932
sink <1468709106@qq.com>
2020-09-09 21:29:11 +0800

    preSum

--- leetcode/差分数组/前缀和.js
+++ leetcode/差分数组/前缀和.js
@@ -8,7 +8,10 @@
  * 输入:nums = [1,1,1], k = 2
  * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
  *
- * 解题思路：前n项和数组
+ * 解题思路：前缀和数组
+ * 前缀和数组preSum的含义：preSum[i]就是nums[0..i-1]的和。
+ * 如果想求nums[i..j]的和，只需要一步操作preSum[j+1]-preSum[i]即可，
+ * 而不需要重新去遍历数组
  * 
  * 时间复杂度O(n^2)
  * 空间复杂度O(n)
@@ -19,17 +22,18 @@
  */
 var subarraySum = function(nums, k) {
     let n = nums.length;
-    // 前n项和数组
+    // 前缀和数组
     let preSum = [];
     preSum[0] = 0;
     for (let i = 1; i <= n; i++) {
         preSum[i] = preSum[i - 1] + nums[i - 1];
     }
     
+    // 符合条件的个数
     let count = 0;
     for (let j = 1; j <= n; j++) {
         for (let i = 0; i < j; i++) {
-            // 区间[i,j]的和等于k
+            // 前缀和相减代表区间[i,j]的和等于k
             if (preSum[j] - preSum[i] === k) {
                 count++;
             }
@@ -37,3 +41,4 @@ var subarraySum = function(nums, k) {
     }
     return count;
 };
+
