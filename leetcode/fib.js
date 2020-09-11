48659f3c0db64ebb45911078f735a5e99b3f2a0b
sinkhaha <1468709106@qq.com>
2020-09-11 21:14:02 +0800

    Update fib.js
    fib2

--- leetcode/fib.js
+++ leetcode/fib.js
@@ -22,3 +22,30 @@ function fib(n) {
     }
     return fib(n-2) + fib(n-1);
 }
+
+/**
+ * 解法二：备忘录递归(自顶向下)
+ * 解法一递归的升级版
+ * 
+ * 比解法一多了个“备忘录”储存，"剪枝"处理技巧，可以去除重复的调用计算
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * 
+ * @param {*} n 
+ */
+function fib2(n) {
+    if (n === 0) {
+        return 0;
+    }
+    const sumMap = {};
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    if (sumMap[n]) {
+        return sumMap[n];
+    }
+    const rst = fib2(n-2) + fib2(n-1);
+    sumMap[n] = rst;
+    return rst;
+}
