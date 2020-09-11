4a703e84ffe8bfa5c5e04def91cf8023c46e7c16
sinkhaha <1468709106@qq.com>
2020-09-11 21:16:34 +0800

    Update coin.js
    coin2

--- leetcode/coin.js
+++ leetcode/coin.js
@@ -35,3 +35,44 @@ function coinChange1(coins, amount) {
 
     return cal(amount);
 }
+
+/**
+ * 解法二：解法一的优化
+ * 备忘录
+ * 
+ * @param {*} coins 
+ * @param {*} amount 
+ */
+function coinChange2(coins, amount) {
+    let subResultMap = {};
+
+    this.cal = function (amount) {
+        if (subResultMap[amount] !== undefined) {
+            return subResultMap[amount];
+        }
+        if (amount === 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return -1;
+        }
+    
+        let result = Infinity;
+        for (let coin of coins) {
+            // 计算子问题
+            const subResult = cal(amount - coin);
+            // 子问题无解
+            if (subResult === -1) {
+                continue;
+            }
+            // 个数为 1 + 子问题的解
+            result = Math.min(result, 1 + subResult);
+        }
+
+        // 备忘录
+        subResultMap[amount] = result == Infinity ? -1 : result;
+        return subResultMap[amount];
+    };
+
+    return cal(amount);
+}
