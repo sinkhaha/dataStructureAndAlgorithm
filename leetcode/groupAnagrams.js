1c7ba6ade59f357d2af576e75f81b6f3b953da9d
sink <1468709106@qq.com>
2020-09-12 13:37:32 +0800

    groupAnagrams

--- leetcode/groupAnagrams.js
+++ leetcode/groupAnagrams.js
@@ -52,19 +52,37 @@ var groupAnagrams1 = function(strs) {
 const strs = ['eat','tea','tan','ate','nat','bat'];
 console.log(groupAnagrams1(strs));
 
+/**
+ * 解法二：按计数分类
+ * 
+ * 思路
+ * 当且仅当它们的字符计数（每个字符的出现次数）相同时，两个字符串是字母异位词。
+ * 
+ * 我们可以将每个字符串s 转换为字符数 count，由26个非负整数组成，表示 a，b，c 的数量等，我们使用这些计数作为哈希映射的基础。
+ *
+ * 跟解法一的区别就是映射的key不同
+ * 
+ * 时间复杂度：O(NK)，N 是 strs 的长度，而 K 是 strs 中字符串的最大长度。
+ * 空间复杂度O(NK)
+ * 
+ * @param {*} strs 
+ */
 var groupAnagrams2 = function(strs) {
     if (strs.length === 0) {
         return [];
     }
-    // key是有序的字符串，value是所有的异位词
+    // key是字符串的计数，value是所有的异位词
     let strCountMap = {};
 
     for (let str of strs) {
+        // 26个非负整数
         let countArr = Array(26).fill(0);
         for (let i = 0; i < str.length; i++) {
            const index = str.charCodeAt(i) - 97;
            countArr[index]++;
         }
+
+        // 用#分隔key，如eat是'1#0#0#0#1#0#0#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0'
         const key = countArr.join('#');
         const value = strCountMap[key];
         if (value === undefined) {
