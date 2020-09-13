26e52eee977a3e902a515749ff875502c05ba51b
lsx <1468709106@qq.com>
2020-09-13 20:40:57 +0800

    maxSumDivThree

--- leetcode/状态机/maxSumDivThree.js
+++ leetcode/状态机/maxSumDivThree.js
@@ -40,9 +40,51 @@ console.log(maxSumDivThree1(nums)); // 12
 
 /**
  * 有限状态机
+ * 
+ * 解题思路：
+ * 1.维护一个3个元素的state数组
+ *  state[0] 表示 mod 为 0 的 最大和
+ *  state[1] 表示 mod 为 1 的 最大和
+ *  state[2] 表示 mod 为 2 的 最大和
+ * 
+ * 2.遍历nums数组的每个元素num
+ * (1)当num%3为0时，state各个元素加上num后，mod 3的值不变
+ * (2)当num%3为1时，
+ * state[2] + num后是一个能被3整除的数，此时只需比较和state[0]哪个大，把值大的赋予state[0]即可,即state[0] = Max(state[2] + num, state[0])
+ * state[0] + num后是一个mod 3为1的数，此时state[1] = Max(state[0] + num, state[1])
+ * state[1] + num后是一个mod 3为2的数，此时state[2] = Max(state[1] + num, state[2])
+ * (3)当num%3为2时，
+ * state[1] + num后是一个能被3整除的数，此时state[0] = Max(state[1] + num, state[0])
+ * state[2] + num后是一个mod 3为1的数，此时state[1] = Max(state[2] + num, state[1])
+ * state[0] + num后是一个mod 3为2的数，此时state[2] = Max(state[0] + num, state[2])
+ * 
+ * 3.最后返回 state[0]即可
+ * 
+ * 时间复杂度O(n)
+ * 空间复杂度O(n)
+ * 
  * @param {number[]} nums
  * @return {number}
  */
 var maxSumDivThree2 = function(nums) {
+    let state = [0, Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
 
+    for (let num of nums) {
+        if (num % 3 === 0) {
+            state = [state[0] + num, state[1] + num, state[2] + num ]
+        } else if (num % 3 === 1) {
+            const a = Math.max(state[2] + num, state[0]);
+            const b = Math.max(state[0] + num, state[1]);
+            const c = Math.max(state[1] + num, state[2]);
+            state = [a, b, c];
+        } else if (num % 3 === 2) {
+            const a = Math.max(state[1] + num, state[0]);
+            const b = Math.max(state[2] + num, state[1]);
+            const c = Math.max(state[0] + num, state[2]);
+            state = [a, b, c];
+        }
+    }
+    return state[0];
 };
+
+console.log(maxSumDivThree2(nums)); // 12
