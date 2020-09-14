1a3f4382b8522d6e9fea1ea6e4d2c32031bf9f5b
lsx <1468709106@qq.com>
2020-09-14 21:18:48 +0800

    maxa

--- leetcode/动态规划/maxA.js
+++ leetcode/动态规划/maxA.js
@@ -1,10 +1,50 @@
-// leetcode 651 四字键盘
+// 
 // 暴力法
+/**
+ * leetcode 651 四字键盘
+ * 
+ * 题目：
+ * 假设你有一个特殊的键盘包含下面的按键：
+ * Key 1: (A)：在屏幕上打印一个 'A'。
+ * Key 2: (Ctrl-A)：选中整个屏幕。
+ * Key 3: (Ctrl-C)：复制选中区域到缓冲区。
+ * Key 4: (Ctrl-V)：将缓冲区内容输出到上次输入的结束位置，并显示在屏幕上。
+ * 现在，你只可以按键 N 次（使用上述四种按键），请问屏幕上最多可以显示几个 'A'呢？
+ * 
+ * 样例 1:
+ * 输入: N = 3
+ * 输出: 3
+ * 解释:
+ * 我们最多可以在屏幕上显示三个'A'通过如下顺序按键：
+ * A, A, A
+ *
+ * 样例 2:
+ * 输入: N = 7
+ * 输出: 9
+ * 解释:
+ * 我们最多可以在屏幕上显示九个'A'通过如下顺序按键：
+ * A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
+ /
+
+/**
+ * 解法一：暴力解法(动态规划)
+ * 
+ * 解题思路：
+ * 1.找【选择】，找【状态】
+ * 2.有4种选择，分别是A、C-A、C-C、C-V（Ctrl简写为C）
+ * 可以定义3个状态：
+ * 第1个状态是剩余的按键次数，用n表示；
+ * 第2个状态是当前屏幕上字 A 的数量，用a_num表示
+ * 第3个状态是剪切板中 A 的数量，用copy表示
+ * 
+ * 3. base case：当剩余次数n为 0 时，a_num就是答案
+ * 
+ * 根据4 种【选择】，把这几种选择通过状态转移表示如下：
+ * (1)选a时，dp(n-1, a_num + 1, copy)
+ * (2)ctrl+v时，dp(n-1, a_num + copy, copy)
+ * (3)ctrl+a ctrl+c时(全选和复制必然是联合使用的)，dp(n-2, a_num, a_num)
+ */
 function maxA1(n) {
-    // 定义3个状态，剩余的按键数/此时a的个数a_num/粘贴版a的个数copy
-    // 选a时， dp(n-1, a_num + 1, copy)
-    // ctrl+v时， dp(n-1, a_num + copy, copy)
-    // ctrl+a ctrl+c时，dp(n-2, a_num, a_num)
     this.dp = function (n, a_num, copy) {
         if (n <= 0) {
             return a_num;
@@ -18,8 +58,10 @@ function maxA1(n) {
         }
     }
 
+    // 初始化时，剩余数为n，a的个数为0，剪切板的个数为0
     return this.dp(n, 0, 0);
 }
+
 const n1 = 3;
 const n2 = 7;
 console.log(maxA1(n1)); // 3
