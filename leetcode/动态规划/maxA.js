dd4f2725bad23374177f334ee6a83fc6d0c979c2
sinkhaha <1468709106@qq.com>
2020-09-14 20:42:37 +0800

    Update maxA.js
    update maxa

--- leetcode/动态规划/maxA.js
+++ leetcode/动态规划/maxA.js
@@ -52,3 +52,48 @@ function maxA2(n) {
 
 console.log(maxA2(n1)); // 3
 console.log(maxA2(n2)); // 9
+
+/**
+ * 只定义一个「状态」，也就是剩余的敲击次数n
+ * 
+ * 定义：dp[i] 表示 i 次操作后最多能显示多少个 A
+ * 
+ * 最优按键序列一定只有两种情况：
+ * (1)要么一直按A：A,A,…A（当 N 比较小时）
+ * (2)要么是这么一个形式：A,A,…C-A,C-C,C-V,C-V,…C-V（当 N 比较大时）
+ * 
+ * 换句话说，最后一次按键要么是A要么是C-V
+ * 
+ * 所以 dp[i] = max(这次按 A 键，这次按 C-V)
+ * 
+ * 对于 [按A键]这种情况，就是状态i - 1的屏幕上新增了一个 A 而已，
+ * 很容易得到结果：dp[i] = dp[i - 1] + 1;
+ * 
+ * 最优的操作序列一定是C-A C-C接着若干C-V，所以我们用一个变量j作为
+ * 若干C-V的起点。那么j之前的 2 个操作就应该是C-A C-C了
+ * 
+ * 时间复杂度 O(N^2)，
+ * 空间复杂度 O(N)
+ * 
+ * @param {*} n 
+ */
+function maxA3(n) {
+    const dp = new Array(n + 1);
+    // 剩余0次则敲0个a
+    dp[0] = 0;
+
+    for (let i = 1; i <= n; i++) {
+        // 按a键
+        dp[i] = dp[i - 1] + 1;
+        // j从2开始，因为c-a加c-c占了两次操作
+        for (let j = 2; j < i; j++) {
+            // 全选+复制 此时的a个数是dp[j-2],连续粘贴i-j次
+            // 屏幕共有dp[j-2]*(i-j+1)个a
+            dp[i] = Math.max(dp[i], dp[j - 2] * (i - j + 1))
+        }
+    }
+    return dp[n];
+}
+
+console.log(maxA3(n1)); // 3
+console.log(maxA3(n2)); // 9
