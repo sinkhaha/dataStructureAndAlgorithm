9655d3d8b01ab39b63921be740292fef1edce58f
sinkhaha <1468709106@qq.com>
2020-09-14 20:29:29 +0800

    Update maxA.js
    update maxA

--- leetcode/动态规划/maxA.js
+++ leetcode/动态规划/maxA.js
@@ -24,3 +24,31 @@ const n1 = 3;
 const n2 = 7;
 console.log(maxA1(n1)); // 3
 console.log(maxA1(n2)); // 9
+
+// 解法一的优化，备忘录
+function maxA2(n) {
+    const map = {}; // key为a-n_num-copy
+    this.dp = function (n, a_num, copy) {
+        if (n <= 0) {
+            return a_num;
+        }
+        for (let i = 0; i < n; i++) {
+            const key = `${n}-${a_num}-${copy}`;
+            if (map[key] !== undefined) {
+                return map[key];
+            }
+            const max = Math.max(
+                dp(n-1, a_num + 1, copy),
+                dp(n-1, a_num + copy, copy),
+                dp(n-2, a_num, a_num)
+            );
+            map[key] = max;
+            return map[key];
+        }
+    }
+
+    return this.dp(n, 0, 0);
+}
+
+console.log(maxA2(n1)); // 3
+console.log(maxA2(n2)); // 9
