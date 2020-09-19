66b4e517c7d9a3d98454d3b288222f34982bc1ce
lsx <1468709106@qq.com>
2020-09-20 00:10:49 +0800

    337

--- leetcode/动态规划/打家劫舍/rob337.js
+++ leetcode/动态规划/打家劫舍/rob337.js
@@ -67,3 +67,28 @@ node2.right = 1;
 console.log(rob1(root)); // 7  因为3 + 3 + 1 = 7
 
 
+var rob2 = function (root) {
+    // 返回⼀个⼤⼩为 2 的数组 arr 
+    // arr[0] 表⽰不抢 root 的话，得到的最⼤钱数
+    // arr[1] 表⽰抢 root 的话，得到的最⼤钱数
+    this.dp = function (root) {
+        if (root == null) {
+            return [0, 0];
+        }
+
+        let left = this.dp(root.left);
+        let right = this.dp(root.right);
+
+        // 抢，下家就不能抢了
+        let do_it = root.val + left[0] + right[0];
+        // 不抢，下家可抢可不抢，取决于收益⼤⼩
+        let not_do_it = Math.max(left[0], left[1])
+            + Math.max(right[0], right[1]);
+        return [not_do_it, do_it];
+    }
+
+    let res = this.dp(root);
+    return Math.max(res[0], res[1]);
+};
+
+console.log(rob2(root)); // 7  因为3 + 3 + 1 = 7
