3d9eb1b5b82b0aa7f1d2e4e45442de44f06e8323
lsx <1468709106@qq.com>
2020-09-24 21:14:28 +0800

    shuffle

--- leetcode/洗牌算法/shuffle.js
+++ leetcode/洗牌算法/shuffle.js
@@ -1,34 +1,46 @@
-// leetcode 384
+/**
+ * leetcode 384 打乱数组
+ * 中等
+ * 
+ * 洗牌算法：产生的结果必须有 n! 种可能
+ * 
+ * 时间复杂度O(n)
+ * 空间复杂度O(1)
+ * 
+ */
 /**
  * @param {number[]} nums
  */
 var Solution = function(nums) {
-    // 保存源数组
-    this.originNums = nums;
-    // 拷贝
-    this.nums = nums.slice(0);
+    this.nums = nums;
 };
 
 /**
+ * 获取原数组
  * Resets the array to its original configuration and return it.
  * @return {number[]}
  */
 Solution.prototype.reset = function() {
-    return this.originNums;
+    return this.nums;
 };
 
 /**
+ * 打乱数组
  * Returns a random shuffling of the array.
  * @return {number[]}
  */
 Solution.prototype.shuffle = function() {
-    const nums = this.nums;
+    const nums = this.nums.slice(0);
     let n = nums.length;
-    for (let i = 0; i < nums.length; i++) {
-        // 从i到尾部随机选一个
+
+    // 产生的结果有 n! 种可能
+    for (let i = 0; i < n; i++) {
+        // 从 i 到 n-1 随机选一个
         const rand = randOne(i, n - 1); 
-        swap(nums, i, rand);
+        // 交换nums数组i和rand下标的两个元素
+        [ nums[i], nums[rand] ] = [ nums[rand], nums[i] ];
     }
+
     return nums;
 };
 
@@ -37,19 +49,12 @@ function randOne(n, m) {
     return Math.floor(Math.random() * (m - n + 1)) + n;
 };
 
-// 交换数组两个值
-function swap(nums, i, j) {
-    const temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-};
-
-const nums = ["Solution","shuffle","reset","shuffle"]
-//[[[1,2,3]],[],[],[]];
+const nums = ['Solution', 'shuffle', 'reset', 'shuffle'];
  var obj = new Solution(nums);
  var param_1 = obj.reset();
  var param_2 = obj.shuffle();
  console.log(param_1);
  console.log(param_2);
+
 
  
