ad52c01b828d58dfb28173f2efdbcac632c11a21
sinkhaha <1468709106@qq.com>
2020-09-27 18:39:15 +0800

    Update findErrorNums.js

--- leetcode/数组/findErrorNums.js
+++ leetcode/数组/findErrorNums.js
@@ -4,6 +4,9 @@
  * 
  * 简单
  * 
+ * 时间复杂度O(n)
+ * 空间复杂度O(1)
+ * 
  * 解法类似448题
  * 
  * @param {number[]} nums
@@ -12,16 +15,19 @@
 var findErrorNums = function(nums) {
     let n = nums.length;
     let result = [];
-    let dup = -1;
+
+    // nums的元素为下标，把对应元素变为负
     for (let i = 0; i < n; i++) {
         const newIndex = Math.abs(nums[i]) - 1;
         if (nums[newIndex] > 0) {
             nums[newIndex] = nums[newIndex] * -1;
         } else {
+            // 已经是负，说明该值重复
             result.push(Math.abs(nums[i]));
         }
     }
 
+    // 正数为缺失的元素
     for (let i = 0; i < n; i++) {
         if (nums[i] > 0) {
             result.push(i + 1);
@@ -29,5 +35,6 @@ var findErrorNums = function(nums) {
     }
     return result;
 };
+
 const nums = [2, 2];
 console.log(findErrorNums(nums)); // [2, 3]
