ffd9e43ceee4a408f7a788593ee1292749ba10c7
lsx <1468709106@qq.com>
2020-09-27 22:20:39 +0800

    45

---
+++ leetcode/贪心算法/jump45.js
@@ -0,0 +1,57 @@
+/**
+ * 45. 跳跃游戏 II
+ */
+/**
+ * 
+ * 动态规划解法
+ * 
+ * dp数组定义：
+ * 从索引 p 跳到最后一格，至少需要 dp(nums, p) 步
+ * 
+ * 选择：在当前索引位置可以跳1到nmus[p]步
+ * 
+ * base case
+ * 当 p 超过最后一格时，不需要跳跃
+ * 
+ * 
+ * 时间复杂度：O(N^2) 递归深度 × 每次递归需要的时间复杂度
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+    let n = nums.length;
+    // 备忘录都初始化为 n，相当于最大值，因为从 0 跳到 n - 1 最多 n - 1 步
+    const memo = Array(n).fill(n);
+
+    this.dp = function (nums, p) {
+        let n = nums.length;
+        // base case , 当 p 超过最后一格时，不需要跳跃
+        if (p >= n - 1) {
+            return 0;
+        }
+
+        if (memo[p] !== n) {
+            return memo[p];
+        }
+
+        // 当前可以选择跳的最多步数
+        let steps = nums[p];
+        // 做选择，计算每一个选择的结果
+        for (let i = 1; i <= steps; i++) {
+            let subProblem = this.dp(nums, p + i);
+            // 最小的为结果
+            memo[p] = Math.min(memo[p], subProblem + 1);
+        }
+        return memo[p];
+    }
+
+    return this.dp(nums, 0);
+};
+
+function test() {
+    const nums = [2, 3, 1, 1, 4];
+    console.log(jump(nums)); // 2
+
+}
+test();
