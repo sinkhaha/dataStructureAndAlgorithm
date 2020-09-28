35ace7019a616b1092d69683612099d429122900
sinkhaha <1468709106@qq.com>
2020-09-28 11:09:57 +0800

    Update getRandom.js

--- leetcode/水塘抽样/getRandom.js
+++ leetcode/水塘抽样/getRandom.js
@@ -1,6 +1,9 @@
 /**
  * 382 链表随机节点
+ * 
  * 中等
+ * 
+ * 类似算法还有398题
  */
 
 function ListNode(val) {
@@ -45,10 +48,42 @@ Solution.prototype.getRandom = function () {
     return res;
 };
 
+/**
+ * 扩展
+ * 返回链表中 k 个随机节点的值
+ * @param {*} k 
+ */
+Solution.prototype.getRandomK = function (k) {
+    let res = new Array(k);
+    let p = this.head;
+
+     // 前 k 个元素先默认选上
+     for (let j = 0; j < k && p != null; j++) {
+        res[j] = p.val;
+        p = p.next;
+    }
+
+    let i = k;
+    while (p != null) {
+        // 生成一个 [0, i) 之间的整数，这个整数等于 0 的概率就是 1/i
+        let j = Math.random() * i;
+        // 这个整数小于 k 的概率就是 k/i
+        if (j < k) {
+            res[j] = p.val;
+        }
+        i++;
+        p = p.next;
+    }
+    return res;
+};
+
 // TODO
 const head = new ListNode(1);
 head.next = new ListNode(2);
 head.next.next = new ListNode(3);
-var obj = new Solution(head)
-var param_1 = obj.getRandom()
+var obj = new Solution(head);
+var param_1 = obj.getRandom();
 console.log(param_1);
+var param_2 = obj.getRandom(2)
+console.log(param_2);
+
