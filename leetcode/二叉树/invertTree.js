e9ecf01526eb9c0612ddba34a428494fa46c2395
sinkhaha <1468709106@qq.com>
2020-09-28 17:12:14 +0800

    Create invertTree.js

---
+++ leetcode/二叉树/invertTree.js
@@ -0,0 +1,31 @@
+/**
+ * 226. 翻转二叉树
+ * 简单
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root == null) {
+        return null;
+    }
+
+    //root节交换左右子节点
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    // 递归
+    invertTree(root.left);
+    invertTree(root.right);
+    
+    return root;
+};
+
+[4,2,7,1,3,6,9];
+
