1a4a7377814fa755dd3acd0682b98388b7e7896e
sinkhaha <1468709106@qq.com>
2020-09-28 17:19:09 +0800

    Create connect116.js

---
+++ leetcode/二叉树/connect116.js
@@ -0,0 +1,51 @@
+/**
+ * 116
+ * 
+ * 中等
+ */
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (root == null) {
+        return null;
+    }
+
+    connectTwoNode(root.left, root.right);
+
+    return root;
+};
+
+/**
+ * 连接两个节点
+ */
+function connectTwoNode(node1, node2) {
+    if (node1 == null || node2 == null) {
+        return;
+    }
+
+    // 前序遍历位置，将传入的两个节点连接
+    node1.next = node2;
+
+    // 递归
+    // 连接相同父节点的两个子节点
+    connectTwoNode(node1.left, node1.right);
+    connectTwoNode(node2.left, node2.right);
+
+    // 连接跨越父节点的两个子节点
+    connectTwoNode(node1.right, node2.left);
+}
+
+// TODO
