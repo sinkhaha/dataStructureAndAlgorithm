0d4fe5f26515842030cafa9fc529e92ad036664f
lsx <1468709106@qq.com>
2020-10-25 17:09:11 +0800

    123

--- leetcode/动态规划/股票问题/maxProfit123.js
+++ leetcode/动态规划/股票问题/maxProfit123.js
@@ -17,29 +17,23 @@ var maxProfit = function (prices) {
     // 最多进行两次交易
     let maxK = 2;
     let dp = [];
-    // 3维dp数组 [n][maxK+1][2];
+    // 初始化3维dp数组为0， [0...n][0..maxK][2]
     for (let i = 0; i < n; i++) {
         dp[i] = [];
-        for (let k = 0; k <= maxK; k++) {
+        for (let k = 0; k < maxK + 1; k++) {
             dp[i][k] = [];
-            for (let s = 0; s < 2; s++) {
-                dp[i][k][s] = 0;
-            }
+            dp[i][k][0] = 0;
+            dp[i][k][1] = 0;
         }
     }
 
-    console.log('dp是：', dp);
-
-    dp[0][0][0] = 0;
-    dp[0][0][1] = -prices[0];
-    dp[0][1][0] = dp[0][1][1] = Number.MIN_SAFE_INTEGER;
-    dp[0][2][0] = dp[0][2][1] = Number.MIN_SAFE_INTEGER;
+    console.log('dp是===：', dp);
 
     for (let i = 0; i < n; i++) {
         for (let k = maxK; k >= 1; k--) {
             // base case
             if (i - 1 == -1) {
-                dp[i][k][0] = 0
+                dp[i][k][0] = 0;
                 dp[i][k][1] = Number.MIN_SAFE_INTEGER;
                 continue;
             }
@@ -50,8 +44,6 @@ var maxProfit = function (prices) {
         }
     }
 
-    console.log(dp);
-
     // 穷举了 n × maxK × 2 个状态，正确
     return dp[n - 1][maxK][0];
 };
@@ -59,16 +51,20 @@ var maxProfit = function (prices) {
 const prices = [3, 3, 5, 0, 0, 3, 1, 4];
 console.log(maxProfit(prices)); // 6
 
+/**
+ * 
+ * @param {*} prices 
+ */
 var maxProfit2 = function (prices) {
     let n = prices.length;
     if (n === 0) {
         return 0;
     }
 
-    let dp_i10 = 0;
-    let dp_i11 = Number.MIN_SAFE_INTEGER;
-    let dp_i20 = 0
-    let dp_i21 = Number.MIN_SAFE_INTEGER;
+    let dp_i10 = 0; // 买了第1个股票又卖了
+    let dp_i11 = Number.MIN_SAFE_INTEGER; // 买了第1个股票，此时还没卖
+    let dp_i20 = 0; // 卖了第2个股票
+    let dp_i21 = Number.MIN_SAFE_INTEGER; // 第2个股票买进来还没有卖
 
     for (let price of prices) {
         dp_i20 = Math.max(dp_i20, dp_i21 + price);
@@ -95,12 +91,13 @@ var maxProfit3 = function (prices) {
     let dp = [];
     for (let i = 0; i < n; i++) {
         dp[i] = [];
-        for (let k = 0; k <= 2; k++) {
+        for (let k = 0; k <= 2; k++) { // 2为最多2次交易
             dp[i][k] = [];
             dp[i][k][0] = 0;
             dp[i][k][1] = 0;
         }
     }
+
     console.log('dp=', dp);
 
     dp[0][0][0] = 0;
@@ -108,6 +105,7 @@ var maxProfit3 = function (prices) {
     dp[0][1][0] = dp[0][1][1] = Number.MIN_SAFE_INTEGER;
     dp[0][2][0] = dp[0][2][1] = Number.MIN_SAFE_INTEGER;
 
+    // 第0天已经初始化，所以从i是1开始
     for (let i = 1; i < n; i++) {
         dp[i][0][0] = dp[i - 1][0][0];
         dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i]);
@@ -116,8 +114,8 @@ var maxProfit3 = function (prices) {
         dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][1][0] - prices[i]);
 
         dp[i][2][0] = Math.max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i]);
-
     }
+
     let end = n - 1;
     return Math.max(dp[end][0][0], dp[end][1][0], dp[end][2][0]);
 }
