b63d08143628f73b51f978060067fd98a5a001bc
lsx <1468709106@qq.com>
2020-10-25 16:33:45 +0800

    121

--- leetcode/动态规划/股票问题/maxProfit122.js
+++ leetcode/动态规划/股票问题/maxProfit122.js
@@ -1,12 +1,15 @@
 /**
- * 122. 买卖股票的最佳时机II
+ * 122. 买卖股票的最佳时机II （无数次交易）
  * 简单
  * 
  * 贪心算法
  * 
+ * 如果当天价格比昨天高，则当天选择买入，计算可得的利润
+ * 如果当天价格比昨天低，则选择不买入
+ * 
  * 时间复杂度：O(N)，N为prices的长度
  * 空间复杂度：O(1)
-
+ *
  * @param {number[]} prices
  * @return {number}
  */
@@ -19,7 +22,7 @@ var maxProfit = function (prices) {
     // 最大利润
     let maxProfit = 0;
     for (let i = 1; i < n; i++) {
-        // 如果当前价格比昨天大，那加上今天可以所得的利润，即每一步都是最佳选择
+        // 如果当前价格比昨天大，则买入，那加上今天可以所得的利润，即每一步都是最佳选择
         if (prices[i] > prices[i - 1]) {
             maxProfit += prices[i] - prices[i - 1];
         }
