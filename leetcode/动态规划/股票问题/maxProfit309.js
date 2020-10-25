943120f3b605a70c78761d5b53ac9cef10dc350e
lsx <1468709106@qq.com>
2020-10-25 20:51:29 +0800

    309

--- leetcode/动态规划/股票问题/maxProfit309.js
+++ leetcode/动态规划/股票问题/maxProfit309.js
@@ -1,3 +1,35 @@
-// TODO
-// 可参考188依然是3维数组， dp[i][k][j]: 此时的k改为0或1即可，表示是否冷冻
+/**
+ * 309. 最佳买卖股票时机含冷冻期
+ * 
+ * 中等
+ */
+/**
+ * 动态规划解法
+ * 
+ * 每次 sell 之后要等一天才能继续交易(跟122的动态规划解法类似)
+ * 
+ * 状态转移方程
+ * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+ * dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i])
+ * 第 i 天选择买入的时候，要从 i-2 的状态转移，而不是 i-1
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let n = prices.length;
+    let dp_i_0 = 0, dp_i_1 = Number.MIN_SAFE_INTEGER;
+    let dp_pre_0 = 0; // 代表 dp[i-2][0]
 
+    for (let i = 0; i < n; i++) {
+        let temp = dp_i_0;
+        dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+        dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i]);
+        dp_pre_0 = temp;
+    }
+
+    return dp_i_0;
+};
+
+const prices = [1, 2, 3, 0, 2];
+console.log(maxProfit(prices)); // 3
