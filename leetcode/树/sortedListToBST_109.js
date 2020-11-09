154a8b87eedf128e0505b1bbf66c306a1c0c3055
lsx <1468709106@qq.com>
2020-11-09 21:48:41 +0800

    sortedlisttobst

---
+++ leetcode/树/sortedListToBST_109.js
@@ -0,0 +1,58 @@
+/**
+ * 109. 有序链表转换二叉搜索树
+ * 
+ * 中等
+ * 
+ * 详细解法见 https://github.com/sinkhaha/my91algo/blob/master/9_sortedListToBST_109.md
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 
+ * 快慢指针 + 递归-分治
+ * 
+ * 时间复杂度O(nlogn)，n是链表的长度
+ * 空间复杂度O(logn)
+ *
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    return buildTree(head, null);
+};
+
+var buildTree = function (head, tail) {
+    if (!head || head === tail) {
+        return null;
+    }
+    
+    // 快慢指针 得到链表中间节点
+    let fast = head;
+    let slow = head;
+    while (fast !== tail && fast.next !== tail) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // 递归构造二叉树
+    let val = slow.val;
+    let root = new TreeNode(val);
+
+    root.left = buildTree(head, slow);
+    root.right = buildTree(slow.next, tail);
+
+    return root;
+}
