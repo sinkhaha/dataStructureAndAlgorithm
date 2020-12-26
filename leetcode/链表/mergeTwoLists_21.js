7b165c4d862a39f09b06b04c745f3876bfacadd7
lsx <1468709106@qq.com>
2020-12-26 14:04:16 +0800

    link

---
+++ leetcode/链表/mergeTwoLists_21.js
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * 21. 合并两个有序链表
+ * @param {*} list1 
+ * @param {*} list2 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 递归
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    if (list1 === null) {
+        return list2;
+    } else if (list2 === null) {
+        return list1;
+    } else if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
